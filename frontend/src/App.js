0da541885da71a9f5e645480148bde45ae982fda
박상민 <iamsangminpark@gmail.com>
2021-11-16 16:10:23 +0900

    초기 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,7 @@
-import logo from "./logo.svg";
-import axios from "axios";
-import "./App.css";
+import React from "react";
 
-function App() {
-  const test = () => {
-    const url = "/test";
-    axios
-      .get(url)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log("failure");
-      });
-  };
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <button onClick={test}>test</button>
-    </div>
-  );
-}
+const App = () => {
+  return <div>Hello World!</div>;
+};
 
 export default App;
