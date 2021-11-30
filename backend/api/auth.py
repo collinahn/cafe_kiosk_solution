38f42e2eff06f4de0d437caf485b2ff3c4f9eb3a
Collin <collinahn@hufs.ac.kr>
2021-11-30 18:05:02 +0900

    [Add] jwt 인증
    로그인 화면에서 POST로 넘어온 데이터를 검증한 후 jwt액세스 토큰을 내려보낸다.

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -5,17 +5,41 @@
 
 from flask import request
 from flask.json import jsonify
+from flask_jwt_extended.view_decorators import jwt_required
 from flask_restx import Api
 from flask_restx import Namespace
 from flask_restx import Resource
-from ..lib.UtilsSE2 import LinkedQueue
+from flask_jwt_extended import create_access_token
+
+from backend.lib.GetPutFmDB import GetPutFmDB
 
 Auth = Namespace('Auth')
+db = GetPutFmDB()
 
-@Auth.route('')
-class AuthTest(Resource):
+@Auth.route('/')
+class CAuth(Resource):
+    dct_Fail: dict = {'success':False}
+
+    def post(self):
+        dct_Input: dict = request.get_json()
+        
+        if len(dct_Input) != 3:
+            return jsonify(self.dct_Fail)
+
+        # 인증 성공!
+        if db.verify_user(dct_Input['actor'], dct_Input['id'], dct_Input['pw']):
+            return jsonify({
+                'success':True,
+                'jwt_token':create_access_token(identity=dct_Input['id'], expires_delta=False)
+            })
+
+        return jsonify(self.dct_Fail)
+
+# thunder-client로 확인 완료
+@Auth.route('/test')
+class CAuthTest(Resource):
+    @jwt_required()
     def get(self):
-
         return jsonify({
-            'auth-ret':True
+            'success':"True"
         })
