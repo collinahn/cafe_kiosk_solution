404787c112e287f10c1086432ab848c97c5925c4
Collin Ahn <87699755+collinahn@users.noreply.github.com>
2021-12-01 22:21:29 +0900

    Feature/api (#5)
    * [Add] jwt 인증
    
    로그인 화면에서 POST로 넘어온 데이터를 검증한 후 jwt액세스 토큰을 내려보낸다.
    
    * [Add] 파라미터 검증
    
    로그인 api에서 파라미터 검증 절차 및 Swagger doc 기능 추가
    함수 위에 expect 데코레이터를 추가함으로써 파라미터 검증 수행(restX 기능)

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -6,16 +6,50 @@
 from flask import request
 from flask.json import jsonify
 from flask_restx import Api
+from flask_restx import fields
 from flask_restx import Namespace
 from flask_restx import Resource
-from ..lib.UtilsSE2 import LinkedQueue
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended.view_decorators import jwt_required
+
+from backend.lib.GetPutFmDB import GetPutFmDB
 
 Auth = Namespace('Auth')
+db = GetPutFmDB()
 
-@Auth.route('')
-class AuthTest(Resource):
+post_body_fields = Auth.model('Resource', {
+    'id':fields.String('id'), 
+    'pw':fields.String('hashed pw'), 
+    'actor':fields.String('staff/admin')
+})
+
+@Auth.route('/')
+@Auth.doc(body=post_body_fields)
+class CAuth(Resource):
+    dct_Fail: dict = {'success':False}
+
+    @Auth.expect(post_body_fields) # 파라미터 검증까지
+    def post(self):
+        dct_Input: dict = request.get_json()
+        
+        if len(dct_Input) != 3:
+            return jsonify(self.dct_Fail)
+
+        # 인증 성공!
+        if db.verify_user(dct_Input['actor'], dct_Input['id'], dct_Input['pw']):
+            return jsonify({
+                'success':True,
+                'jwt_token':create_access_token(identity=dct_Input['id'], expires_delta=False)
+            })
+
+        return jsonify(self.dct_Fail)
+
+
+# thunder-client로 확인 완료
+@Auth.route('/test')
+class CAuthTest(Resource):
+    @jwt_required()
     def get(self):
-
         return jsonify({
-            'auth-ret':True
+            'success':"True"
         })
