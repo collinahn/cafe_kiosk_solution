1fc9d2e0aeba54cd1f43d974263add05a7d737de
박상민 <iamsangminpark@gmail.com>
2021-12-01 17:32:16 +0900

    add: Cart 컴포넌트 복구

--- frontend/src/components/Main/Cart.js
+++ frontend/src/components/Main/Cart.js
@@ -1,12 +1,84 @@
 import React from "react";
 import Slide from "./Slide";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const Cart = ({ menuArray }) => {
+export default function Cart({ menuArray }) {
+  let sum = 0;
+  for (let i = 0; i < menuArray.length; i++) {
+    sum = sum + menuArray[i].price;
+  }
+
   return (
-    <div>
-      <Slide menuArray={menuArray} />
-    </div>
+    <>
+      <CartWrap>
+        <TitleWrap>
+          <TextWrap>장바구니</TextWrap>
+          <PriceWrap>총 주문금액 ₩{sum}</PriceWrap>
+        </TitleWrap>
+        <Slide menuArray={menuArray} />
+      </CartWrap>
+      <BottomButtonWrap>
+        <LinkWrap to="/">
+          <CloseButtonWrap>취소하기</CloseButtonWrap>
+        </LinkWrap>
+        <LinkWrap to="/pay">
+          <PayButtonWrap>결제하기</PayButtonWrap>
+        </LinkWrap>
+      </BottomButtonWrap>
+    </>
   );
-};
+}
 
-export default Cart;
+const CartWrap = styled.div`
+  border-top: 2px solid black;
+  background-color: #f6f6ff;
+`;
+
+const TitleWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  color: #493e3e;
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+const LinkWrap = styled(Link)`
+  width: 100%;
+  text-decoration: none;
+`;
+
+const BottomButtonWrap = styled.div`
+  border-top: 2px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-weight: bold;
+`;
+
+const CloseButtonWrap = styled.div`
+  background-color: black;
+  color: white;
+  padding: 5px 0;
+  border: 2px solid black;
+`;
+
+const PayButtonWrap = styled.div`
+  color: #493e3e;
+  padding: 5px 0;
+`;
+
+const TextWrap = styled.p`
+  margin: 15px 0 15px 50px;
+  font-weight: bold;
+  font-size: 16px;
+  margin-left: 50px;
+`;
+const PriceWrap = styled.p`
+  margin: 15px 50px 15px 0;
+  font-weight: bold;
+  font-size: 16px;
+`;
--- frontend/src/pages/Manager.js
+++ frontend/src/pages/Manager.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../components/common/Header";
-import Toggle from "../components/common/Toggle";
 import MenuList from "../components/Manager/MenuList";
 import AddMenu from "../components/Manager/AddMenu";
 import "../assets/css/Manager.css";
@@ -9,7 +8,6 @@ const Manager = () => {
   return (
     <>
       <Header />
-      {/* <Toggle MenuArray={["커피", "에이드", "베이커리", "디저트"]} /> */}
       <MenuList />
       <AddMenu />
     </>
