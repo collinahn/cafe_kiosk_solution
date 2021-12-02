9c77c001d940599dd9bd00f9d657401e8a6f225f
Collin Ahn <collinahn@hufs.ac.kr>
2021-12-02 15:31:55 +0900

    [Add] 주문 초기화면 아이템 정보 GET api
    api추가 및 internal server error 나오지 않도록 기존 라이브러리 수정

--- backend/lib/Items.py
+++ backend/lib/Items.py
@@ -19,7 +19,7 @@ class Items(object):
     __mset_MstCode: set[str] = set() # 초기화 기록용
     __mset_MstCategory: set[str] = set() # 상품코드 추적
 
-    def __new__(cls, tplItemInfo: tuple, sItemCode: str=None, *args):
+    def __new__(cls, tplItemInfo: tuple, sItemCode: str=None, bTraceCategory: bool=True, *args):
         #튜플로 생성, str으로 조회
         if sItemCode and sItemCode in cls.__mdct_MstInstance: # 아이템 코드로 호출
             cls._instance = cls.__mdct_MstInstance[sItemCode]
@@ -48,7 +48,7 @@ class Items(object):
             self._s_Category = tpleItemInfo[2]
             self._n_Price = tpleItemInfo[3]
             self._n_Time = tpleItemInfo[4]
-            self._b_Avail = ( tpleItemInfo[5] == 0 )
+            self._b_Avail = ( tpleItemInfo[5] != 0 )
             self._s_Url = tpleItemInfo[6]
 
             self.logger.INFO(f"Item {self._s_Code} init")
@@ -68,7 +68,7 @@ class Items(object):
     @classmethod
     def get_categories(cls) -> set:
         # 보유중인 카테고리를 확인
-        return cls.__mset_MstCategory
+        return list(cls.__mset_MstCategory)
 
     @property
     def code(self) -> str:
