ee80d79d4fd550e5ad02b00237e84b9f036debf2
Collin Ahn <87699755+collinahn@users.noreply.github.com>
2021-12-02 15:08:46 +0900

    [Add] ItemFactory 클래스 및 기능 (#11)
    Item인스턴스를 끌어다 사용하기 쉽게 한 곳에서 생성하고 관리한다.
    lazy_init()으로 초기화하고 get_instance(sItemCode)로 개별 Items 객체를 불러와 사용한다.
    이 경우 sItemCode가 올바르지 않다면 None을 리턴하는 것이 아닌 빈 Items객체를 리턴한다.
    상품 목록이 전부 필요하다면 cls_It.asset 과 같은 식으로 호출하면 dict형으로 리턴한다.

--- backend/lib/Items.py
+++ backend/lib/Items.py
@@ -19,7 +19,7 @@ class Items(object):
     __mset_MstCode: set[str] = set() # 초기화 기록용
     __mset_MstCategory: set[str] = set() # 상품코드 추적
 
-    def __new__(cls, tplItemInfo: tuple, sItemCode: str=None):
+    def __new__(cls, tplItemInfo: tuple, sItemCode: str=None, *args):
         #튜플로 생성, str으로 조회
         if sItemCode and sItemCode in cls.__mdct_MstInstance: # 아이템 코드로 호출
             cls._instance = cls.__mdct_MstInstance[sItemCode]
@@ -39,7 +39,7 @@ class Items(object):
         cls.logger.INFO(cls._instance)
         return cls._instance
 
-    def __init__(self, tpleItemInfo: tuple[str,str,str,int,int,int,str], *args) -> None:
+    def __init__(self, tpleItemInfo: tuple[str,str,str,int,int,int,str], *args, bTraceCategory: bool=True) -> None:
         cls = type(self)
         if tpleItemInfo[0] not in cls.__mset_MstCode:
 
@@ -54,7 +54,8 @@ class Items(object):
             self.logger.INFO(f"Item {self._s_Code} init")
 
             cls.__mset_MstCode.add(self._s_Code)
-            cls.__mset_MstCategory.add(self._s_Category) # 카테고리 추적
+            if bTraceCategory: # 더미 데이터 거르기 위함
+                cls.__mset_MstCategory.add(self._s_Category) # 카테고리 추적
 
     def __del__(self):
         print(f"{self.code} collected")
