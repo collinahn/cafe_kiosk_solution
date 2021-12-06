c652691c904c37082c8f9629755fdf9bb4aca925
DESKTOP-FJ7812N\이혜원 <sweetpino10@gmail.com>
2021-12-06 20:02:52 +0900

    [fix] order API (order-post&orderCode-post)

--- backend/api/order.py
+++ backend/api/order.py
@@ -11,20 +11,24 @@ from flask_restx import Resource
 
 import backend.lib.UtilsSE2 as utils
 import backend.lib.constantsSE2 as const
+from backend.lib.LoggerSE2 import Logger
+from backend.lib.Items import Items4Order
+from backend.lib.UtilsSE2 import LinkedQueue
 from backend.lib.OrderManager import OrderManager
 from backend.lib.GetPutFmDB import GetPutFmDB
 
+log = Logger()
 Order = Namespace('Order')
 cls_Om = OrderManager()
+cls_Q = LinkedQueue()
 db = GetPutFmDB()
 
-class itemOrder(fields.Raw):
-    def format(self):
-        order = {'itemCode':fields.String('itemCode'),
-                 'quantity':fields.Integer(100)}
-        return order
-
-post_order_body = Order.model('Resource-Order', {'order':itemOrder(attribute = 'itemOrder')})
+post_order_body = Order.model('Resource-Order', {
+    'order':fields.Raw({
+        'cake001':1,
+        'drink001':100
+        })
+    })
 
 cancel_order_body = Order.model('Resource-Cancel',{'isCancel':fields.Boolean(True)})
 
@@ -35,12 +39,24 @@ class COrder(Resource):
     @Order.expect(post_order_body)
     def post(self):
         dct_Input: dict = request.get_json()
-        cls_Q = utils.LinkedQueue()
-        return jsonify({'success':True,
-                        'orderCode':cls_Om.push_order(dct_Input['order']),
-                        'timeComplete':cls_Q.estimate()
-                        })
 
+        # 올바르지 않은 요청일 때 필터링
+        try:
+            if not dct_Input['order']:
+                return jsonify(const.SUCCESS_FALSE_RESPONSE)
+        except KeyError as e:
+            log.ERROR('key error no key named', e, 'instead received', dct_Input.keys())
+            return jsonify(const.SUCCESS_FALSE_RESPONSE)
+
+        # 주문을 분해해서 Items4Order객체로 만든 뒤, 그 다음에 주문을 푸시한다
+        tpl_Order: tuple = (Items4Order(key, value) for key, value in dct_Input['order'].items() )
+        s_OrderNo: str = cls_Om.push_order(tpl_Order)
+
+        return jsonify({
+            'success':True,
+            'orderCode':s_OrderNo,
+            'timeComplete':cls_Q.estimate()
+        })
 
 @Order.route('/<orderCode>')
 class COrderCode(Resource):
@@ -57,7 +73,11 @@ class COrderCode(Resource):
         dct_Input: dict = request.get_json()
 
         b_CnlRequest = dct_Input['isCancel']
-        cls_Q = utils.LinkedQueue()
+
+        #주문서에 주문 번호가 없음
+        if not utils.to_int(orderCode):
+            return jsonify(const.SUCCESS_FALSE_RESPONSE)
+        
         if b_CnlRequest:
             return jsonify({
                 'isCancellable':cls_Q._check_cancellable(int(orderCode)),
