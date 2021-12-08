5f91e8912ef030e431383541742e36ce2a8c44e8
박상민 <iamsangminpark@gmail.com>
2021-12-08 22:45:05 +0900

    feat: 주문 과정 페이지 추가 구현

--- frontend/src/components/common/MenuList.js
+++ frontend/src/components/common/MenuList.js
@@ -1,14 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
 import Americano from "../../assets/image/Americano.svg";
-import AppleAde from "../../assets/image/AppleAde.svg";
-import GreenAppleAde from "../../assets/image/GreenAppleAde.svg";
-import Dacquoise from "../../assets/image/Dacquoise.svg";
-import ButterDacquoise from "../../assets/image/ButterDacquoise.svg";
-import ChocoDacquoise from "../../assets/image/ChocoDacquoise.svg";
-import CreamDacquoise from "../../assets/image/CreamDacquoise.svg";
-import PinkSaltDacquoise from "../../assets/image/PinkSaltDacquoise.svg";
-import StrawberryDacquoise from "../../assets/image/StrawberryDacquoise.svg";
-import TiramisuDacquoise from "../../assets/image/TiramisuDacquoise.svg";
 import { Link } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -16,13 +7,11 @@ import Slide from "../Main/Slide";
 import styled from "styled-components";
 import axios from "axios";
 
-const TitleArray = ["케이크", "음료수", "주류"];
-
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://127.0.0.1:5000";
 
 export default function MenuList() {
-  const [menu, setMenu] = useState(TitleArray[0]);
+  const [classification, setClassification] = useState([]);
   const [APImenu, setAPImenu] = useState([]);
   // API에서 값 받아오기
   useEffect(() => {
@@ -30,7 +19,9 @@ export default function MenuList() {
       .get("/start/v1")
       .then((res) => {
         setAPImenu(res.data.data);
+        setClassification(res.data.classification);
         console.log("불러오기 성공");
+        console.log(res.data);
         console.log(APImenu);
       })
       .catch((err) => console.error(err))
@@ -66,68 +57,46 @@ export default function MenuList() {
     });
   }, []);
 
-  const onSubmit = ({ e, props }) => {
-    e.preventDefault();
-    try {
-      const data = cart;
-      axios
-        .post("/order/", JSON.stringify(data), {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-        .then((res) => {
-          console.log(res);
-          console.log(res.data);
-          console.log("전송완료");
-          props.history.push("/pay");
-        })
-        .catch((e) => {
-          console.log("fail because of" + e);
-        })
-        .finally((e) => {
-          console.log("sending process over");
-        });
-    } catch (e) {
-      console.log(e);
-    }
-  };
   let sum = 0;
   for (let i = 0; i < cart.length; i++) {
     sum = sum + cart[i].itemPrice;
   }
 
-  console.log(cart);
-  const CakeList = APImenu.filter((data) => {
-    return data.itemClass === "cake";
+  const BrunchList = APImenu.filter((data) => {
+    return data.itemClass === "브런치";
+  });
+
+  const DeesertList = APImenu.filter((data) => {
+    return data.itemClass === "디저트";
+  });
+
+  const CoffeeList = APImenu.filter((data) => {
+    return data.itemClass === "커피";
   });
 
   const DrinkList = APImenu.filter((data) => {
-    return data.itemClass === "drink";
+    return data.itemClass === "음료";
   });
 
-  const AlcoholList = APImenu.filter((data) => {
-    return data.itemClass === "alcohol";
-  });
-
-  let [targetArray, setTargetArray] = useState(CakeList);
+  let [targetArray, setTargetArray] = useState(BrunchList);
 
   const handleButtonClick = (e) => {
-    setMenu(e.target.value);
     console.log(e.target.value);
-    if (e.target.value === "케이크") {
-      setTargetArray(CakeList);
-    } else if (e.target.value === "음료수") {
+    if (e.target.value === "브런치") {
+      setTargetArray(BrunchList);
+    } else if (e.target.value === "디저트") {
+      setTargetArray(DeesertList);
+    } else if (e.target.value === "커피") {
+      setTargetArray(CoffeeList);
+    } else if (e.target.value === "음료") {
       setTargetArray(DrinkList);
-    } else if (e.target.value === "주류") {
-      setTargetArray(AlcoholList);
     }
   };
 
   return (
     <>
       <TitleWrap>
-        {TitleArray.map((menu, idx) => {
+        {classification.map((menu, idx) => {
           return (
             <MenuBtnLabel id="menu" key={idx}>
               <InputWrap
@@ -154,7 +123,7 @@ export default function MenuList() {
               <MenuWrap onClick={onAdd} key={menu.id} id={menu.id}>
                 <img
                   style={{ width: "43px", height: "61px" }}
-                  src={thumbnail}
+                  src={Americano}
                   alt={itemName}
                 />
                 <MenuNameWrap>{itemName}</MenuNameWrap>
@@ -177,13 +146,19 @@ export default function MenuList() {
         <LinkWrap to="/">
           <CloseButtonWrap>취소하기</CloseButtonWrap>
         </LinkWrap>
-        <SubmitButtonWrap obSubmit={onSubmit}>
+        <LinkWrap
+          to={{
+            pathname: "/order",
+            props: cart,
+          }}
+        >
           <PayButtonWrap>결제하기</PayButtonWrap>
-        </SubmitButtonWrap>
+        </LinkWrap>
       </BottomButtonWrap>
     </>
   );
 }
+
 const CartWrap = styled.div`
   border-top: 2px solid black;
   background-color: #f6f6ff;
@@ -197,15 +172,6 @@ const CartTitleWrap = styled.div`
   color: #493e3e;
   font-size: 16px;
   font-weight: 500;
-`;
-
-const SubmitButtonWrap = styled.button`
-  width: 100%;
-  text-decoration: none;
-  border: none;
-  background-color: transparent;
-  font-weight: bold;
-  font-size: 16px;
 `;
 
 const LinkWrap = styled(Link)`
@@ -248,6 +214,11 @@ const PriceWrap = styled.p`
 
 const MenuListWrap = styled.div`
   height: 322px;
+  overflow: scroll;
+  -ms-overflow-style: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
   padding-top: 15px;
   display: flex;
   flex-wrap: wrap;
@@ -265,23 +236,6 @@ const MenuNameWrap = styled.p`
   text-align: center;
   font-size: 11px;
   margin: 0;
-`;
-
-const EmptyMenuWrap = styled.div`
-  width: 86px;
-  height: 81px;
-  background-color: #f6f6ff;
-  border: 1px solid #493e3e;
-  box-shadow: inset 0px 3px 3px #aaa;
-  border-radius: 10px;
-  text-align: center;
-  margin: 0 3px;
-`;
-
-const EmptyMenuNameWrap = styled.p`
-  font-weight: bold;
-  font-size: 24px;
-  margin-top: 27px;
 `;
 
 const TitleWrap = styled.div`
