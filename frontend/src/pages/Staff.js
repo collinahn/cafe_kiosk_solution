18877fd57a223e528dc4405be1942b6a7c35d241
박상민 <iamsangminpark@gmail.com>
2021-12-10 22:13:59 +0900

    feat: 새로운 Header 컴포넌트 반영

--- frontend/src/pages/Staff.js
+++ frontend/src/pages/Staff.js
@@ -1,22 +1,12 @@
 import React from "react";
-import Header from "../components/common/Header";
+import Header from "../components/Staff/Header";
 import OrderedList from "../components/Staff/OrderedList";
-import styled from "styled-components";
 
 export default function Staff() {
   return (
     <>
       <Header />
-      <BodyWrap>&nbsp;&nbsp;&nbsp;주문&nbsp;현황</BodyWrap>
       <OrderedList />
     </>
   );
 }
-
-const BodyWrap = styled.p`
-  text-shadow: 1px 1px 1px #493e3e;
-  font-size: 24px;
-  font-weight: bold;
-  margin: 0px 0 10px 0;
-  color: #493e3e;
-`;
